d69ad88e333ef442c2a4c3e5d776e1bc7be533ae
Juan Castro <programador20@tecnologicocomfenalco.edu.co>
2025-03-28 16:48:54 -0500

    Seeder para crear roles y permisos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call([UserSeeder::class]);
      
 
         Category::factory(5)->create();
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,10 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserSeeder extends Seeder
@@ -30,13 +31,25 @@ class UserSeeder extends Seeder
 
        $adminUser = User::factory()->create([
             'name' => 'Carlos De la rosa',
-            'email' => 'carlitos@gmail.com',
+            'email' => 'test@example.com',
+            'password' => Hash::make('12345678'),
             'password' => bcrypt('12345678')
         ]);
 
         $roleAdmin = Role::create(['name' => 'administrator']);
         $adminUser -> assignrole($roleAdmin);
         $roleAdmin->syncPermissions($permissionAdmin);
+
+        $desmpleadouser = User::factory()->create([
+            'name' => 'Juan Castro',
+            'email' => 'test2@example.com',
+            'password' => Hash::make('12345678'),
+            'password' => bcrypt('12345678')
+        ]);
+
+        $roledesempleado = Role::create(['name' => 'desempleado']);
+        $desmpleadouser -> assignrole($roledesempleado);
+        $roledesempleado->syncPermissions($permissionAdmin);
 
 
     }
