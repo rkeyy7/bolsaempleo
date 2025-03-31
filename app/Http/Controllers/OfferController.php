6703d4aedb1a4621c951274af017fd8de912cb88
JuanWF <shispeca57@gmail.com>
2025-03-31 11:31:50 -0500

    Agregado OfferController, migración de applications y vista de ofertas

---
+++ app/Http/Controllers/OfferController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Offer;
+
+class OfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $offers = Offer::with('user')->latest()->paginate(10);
+        
+        return view('offers.index',['offers' => $offers]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('offers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        {
+            request()->validate([
+                'title' => ['required', 'min:3'],
+                'salary' => ['required']
+            ]);
+    
+            $offer = Offer::create([
+                'enterprise_name' => request('enterprise_name'),
+                'title' => request('title'),
+                'description' => request('description'),
+                'salary' => request('salary'),
+                'location' => request('location'),
+                // 'user_id' => 1
+            ]);
+    
+            return redirect('/offers')
+                ->with('status', 'Offer created successfully!');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Offer $offer)
+    {
+        return view('offers.show', ['offer' => $offer]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
